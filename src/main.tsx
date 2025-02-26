1bb0829128038f448e9d7b74cb5c1ba024cd3b2b
arthay <arthur15251@gmail.com>
2025-02-26 14:51:21 +0400

    App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "@/App";
 
-import enMessages from "@/assets/translations/en.json";
-
-import './index.css'
-import routes from "@/routes";
-import { IntlProvider } from "react-intl";
-import { Toaster } from "@/components/ui/sonner";
-
-const locale = 'en';
-
-const queryClient = new QueryClient();
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      {import.meta.env.DEV && (
-        <ReactQueryDevtools initialIsOpen={false} />
-      )}
-      <IntlProvider messages={enMessages} locale={locale}>
-        <RouterProvider router={routes} />
-        <Toaster />
-      </IntlProvider>
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 )
